628617fdf90de376d4a3ce130ad7b8590dc5ad48
Gonçalo Alexandre Gongalves Alves <goncalogalves@outlook.com>
2024-10-24 17:02:02 +0100

    register

--- app/src/main/java/com/example/todotask/MainActivity.kt
+++ app/src/main/java/com/example/todotask/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.todotask
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todotask.ui.theme.TodotaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +40,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box (modifier=Modifier.fillMaxSize().padding(80.dp)) {
+        Text(
+            text="Register",
+            color= Color.Black,
+            fontSize = 30.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
